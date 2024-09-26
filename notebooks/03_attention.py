fbd60c2c65450d4fc695ab40b1a79a3a3239837c
Steve Schmerler <git@elcorto.com>
2024-09-26 12:33:51 +0200

    jupytext: sync notebooks and py files
    See Makefile: Sync by using
    
        jupytext --set-formats
    
    once.

--- notebooks/03_attention.py
+++ notebooks/03_attention.py
@@ -1,11 +1,12 @@
 # ---
 # jupyter:
 #   jupytext:
+#     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.16.3
+#       jupytext_version: 1.16.4
 #   kernelspec:
 #     argv:
 #     - python
