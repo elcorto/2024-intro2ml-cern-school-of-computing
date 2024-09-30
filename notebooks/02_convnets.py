05d4cb8e3962eb8c63acc74191830c4e7bff8338
Sebastian Starke <s.starke@hzdr.de>
2024-09-30 11:05:25 +0200

    Fix to py:percent

--- notebooks/02_convnets.py
+++ notebooks/02_convnets.py
@@ -3,8 +3,8 @@
 #   jupytext:
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
+#       format_name: percent
+#       format_version: '1.3'
 #       jupytext_version: 1.16.2
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
@@ -12,6 +12,7 @@
 #     name: python3
 # ---
 
+# %%
 r"""
 # Convolutional Neural Networks
 
@@ -26,7 +27,7 @@ Take out your phone. Check in the picture galery some of the last images you too
 Imagine you want to design an MLP network to categorize the pictures you took on your camera. We convert each image into a flat vector before feeding it into the network. We assume you like to use three hidden layers with 128 hidden neurons each. Then (for sake of the example) you want to predict a single category, i.e. your last layer has a size of 1. In this scenario, compute how many parameters such an MLP might have.
 """
 
-# + [markdown] jupyter={"source_hidden": true}
+# %% [markdown] jupyter={"source_hidden": true}
 # ### 02.1 Solution
 #
 # On my phone, I go to "Gallery", then select a picture. Then I "click" on properties (some phones call this information) and see that it has 2160 pixels in width and 3840 pixels in height (`2160x3840`). We ignore for the time being that most of these images are encoded as RGB or RGBA or even something else.
@@ -45,7 +46,7 @@ Imagine you want to design an MLP network to categorize the pictures you took on
 #
 # In sum, this network would have $1061716481$ parameters. As each trainable parameter in a pytorch model is typically a `float32` number. This would result in the model to be of size $1,061,716,481 \cdot 4 \text{Byte} = 4,246,865,924 \text{Byte} \approx 3.9 \text{GiB} \approx 4.2 \text{GB}$. Such a model would already exceed some GPU's memory. So we better look for a way to have neural networks with a smaller number of parameters.
 
-# + [markdown]
+# %% [markdown]
 """
 ## A note on reproducibility
 
@@ -58,7 +59,7 @@ In the following code, we will rely a lot on [pseudorandom number generators](ht
 For didactical purposes, we fix the pseudorandomness to certain seed values (i.e. we fix the beginning of the random sequence). This will avoid more confusion than necessary when comparing results.
 """
 
-# +
+# %%
 
 import numpy as np
 import torch
@@ -66,7 +67,7 @@ import torch
 np.random.seed(13)
 torch.random.manual_seed(12)
 
-# + [markdown]
+# %% [markdown]
 """
 ## Loading 1D Training Data
 
@@ -75,7 +76,7 @@ To explore convolutions, we will start out considering a one dimensional sequenc
 <div style="display: block;margin-left: auto;margin-right: auto;width: 75%;"><img src="https://github.com/greydanus/mnist1d/raw/master/static/overview.png" alt="MNIST1D overview"></div>
 """
 
-# +
+# %%
 from mnist1d.data import get_dataset_args, make_dataset
 
 defaults = get_dataset_args()
@@ -83,11 +84,11 @@ data = make_dataset(defaults)
 X, y = data['x'], data['y']
 
 X.shape, y.shape, type(X), X.dtype, y.dtype
-# -
 
+# %% [markdown]
 # As we are interested in supervised learning, we rely on pairs of input data `X` and labels `y`. Here, the labels `y` refer to the number which each sequence in `X` resembles. Let's have a look at these sequences.
 
-# +
+# %%
 import matplotlib.pyplot as plt
 
 f, ax = plt.subplots(2, 5, figsize=(14, 5), sharex=True, sharey=True)
@@ -105,11 +106,11 @@ for sample in range(10):
 
 f.suptitle("MNIST1D examples")
 f.savefig("mnist1d_default_first10.svg")
-# -
 
+# %% [markdown]
 # You can tell from the above, that the signals are far from easy to distinguish. This gives rise for the need for a flexible network architecture. But let's not get ahead of ourselves. Before we dive into designing a network, we will have to create a dataloader first. In `pytorch`, having a `DataLoader` for your data is the first hurdle you have to overcome to get started.
 
-# +
+# %%
 from typing import Callable
 
 import numpy as np
@@ -163,15 +164,14 @@ class MNIST1D(torch.utils.data.Dataset):
         return X, y
 
 
-# -
-
+# %% [markdown]
 # In `pytorch`, the Dataset class has to comply to 3 requirements:
 # - it has to inherit from torch.utils.data.Dataset
 # - it has to define a `__len__` function so that we can later call `len(mydataset)`
 # - it has to define a `__getitem__` function so that we can later call `mydataset[12]` to obtain sample 12
 # For more details, see the `pytorch` documentation on [Creating a Custom Dataset for your files](https://pytorch.org/tutorials/beginner/basics/data_tutorial.html#creating-a-custom-dataset-for-your-files) or [Datasets & DataLoaders](https://pytorch.org/tutorials/beginner/basics/data_tutorial.html).
 
-# +
+# %%
 training_data = MNIST1D()
 validation_data = MNIST1D(train=False)
 
@@ -183,11 +183,11 @@ assert validationx.shape[-1] == 40, f"x shape should end with 40 but got:{valida
 assert validationy.item() == 5, f"y should be 5 but got {validationy.item()}"
 assert isinstance(validationx, torch.Tensor)
 assert isinstance(validationy, torch.LongTensor)
-# -
 
+# %% [markdown]
 # In order to use the dataset for training, we need to create a DataLoader. A DataLoader orchestrates how the data is loaded and provided for the compute device that you intend to use. Note how we can set how many MNIST1D sequences at once will be provided to the compute device. This number, called the **batch size**, is set to `64` in the example below.
 
-# +
+# %%
 from torch.utils.data import DataLoader
 
 batch_size = 64
@@ -199,8 +199,7 @@ train_X, train_y = next(iter(train_dataloader))
 print("Batch of training data and labels with shapes",train_X.shape, train_y.shape)
 
 
-# -
-
+# %% [markdown]
 # ## Convolutions in 1D
 #
 # Up until here, we have made great strides to load our dataset. We now want to explore what convolutions are and how to use them to construct a convolutional neural network (CNN).
@@ -238,6 +237,7 @@ print("Batch of training data and labels with shapes",train_X.shape, train_y.sha
 #
 # We will now construct a model in Pytorch. The setup has to comply to some rules. But let's dive in first.
 
+# %%
 class MyCNN(torch.nn.Module):
 
     def __init__(self, nlayers: int = 3, nchannels=16):
@@ -279,18 +279,22 @@ class MyCNN(torch.nn.Module):
 
         return sum([p.numel() for p in self.parameters() if p.requires_grad])
 
+# %% [markdown]
 # Again, the model definition above has to comply to some rules:
 # - the model has to inherit from `torch.nn.Module`
 # - the model has to implement `__init__`
 # - the model has to implement a `forward` function
 # We can check that we have implemented the model "correctly", but just passing in some data. All weights are initialized randomly by default.
 
+# %%
 model = MyCNN(nchannels=32) # construct the model
 
+# %%
 with torch.no_grad():
     output = model(train_X) # perform a forward pass (note the __call__ method is automatically using the forward function)
 print(output.shape)
 
+# %% [markdown]
 # ## **Exercise 02.2** How does my model look like?
 #
 # When designing a model, it can be very easy to loose track of what you are doing. In this exercise, we want to explore how to inspect models. `torch` only has one built-in way to display the structure of a model. Try the following code:
@@ -299,18 +303,18 @@ print(output.shape)
 #
 # Doing so can also serve as another first validation, if you have implemented the model with respect to python syntax correctly.
 
-# + jupyter={"source_hidden": true}
+# %% jupyter={"source_hidden": true}
 # Solution 02.2
 print(model)
-# -
 
+# %% [markdown]
 # ## **Exercise 02.3** More than just __repr__!
 #
 # Printing a model is nice and provides a first glance at the structure of your architecture. In practice however, there is one more thing that is crucial in designing a torch model. You will notice with time how essential it is to understand how data flows through a model! For this, `torch` does **NOT** have built-in methods.
 #
 # At the time of writing, there is a nice library called `torchinfo` available on [github](https://github.com/TylerYep/torchinfo) and [pypi](https://pypi.org/project/torchinfo/). Your task is to install this library and run the summary function with the `model` above.
 
-# + [md] jupyter={"source_hidden": true}
+# %% [md] jupyter={"source_hidden": true}
 # Solution 02.3, install the library if not present already
 #
 # Run the following code in a cell to install the `torchinfo` package:
@@ -318,29 +322,33 @@ print(model)
 # python -m pip install torchinfo
 # ```
 
-# + jupyter={"source_hidden": true}
+# %% jupyter={"source_hidden": true}
 # Solution 02.3, second use the library
 from torchinfo import summary
 
 summary(model, input_size=train_X.shape)
-# -
 
+# %% [markdown]
 # ## Classification with a CNN
 #
 # CNNs have become extremely popular to use for classification tasks. A classification tries to categorize or classify a given input signal to a fixed number of possible outcomes. In our case, these outcomes are the class labels of MNIST1D. For this reason, we can call our model a classifier now. We have to create an optimizer that takes care of updating the model parameters in a smart way exploiting the gradients of the loss function. We use `AdamW` with default options for the time being. Note, to construct the optimizer, we have to provide all parameters of our model in the constructor so it knows which parameters to adjust.
 
+# %%
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-2)
 
+# %% [markdown]
 # As a next step, we have to consider the loss function. To do so, we will use the CrossEntropyLoss.
 #
 # According to the documentation, this loss functions expects predictions to be unnormalized "logits" and internally applies the softmax activation to obtain class probabilities in a numerically efficient way. This is the reason why our model did not include this activation in the forward step.
 #
 
+# %%
 criterion = torch.nn.CrossEntropyLoss() # our loss function
 
+# %% [markdown]
 # We are now ready to code up our training loop.
 
-# +
+# %%
 from sklearn.metrics import accuracy_score as accuracy
 
 max_epochs = 60
@@ -402,19 +410,19 @@ for epoch in range(max_epochs):
     if epoch % log_every == 0 or (epoch+1) == max_epochs:
         print(f"{epoch+1}/{max_epochs} :: training loss {train_loss.mean()} accuracy {train_acc.mean()}; validation loss {validation_loss.mean()} accuracy {validation_acc.mean()}")
 
-# + [markdown]
+# %% [markdown]
 """
 You can tell from the above that pytorch is yet a very low level library. You have to code up the training for-loop yourself. This is in contrast to other libraries like [keras](https://keras.io), [pytorch-lightning](https://lightning.ai/docs/pytorch/stable/) or [scikit-learn](https://scikit-learn.org). These libraries provide you with complete "Trainer" or model-like objects which in turn offer a `.fit` method to start training. Decide on your own which suits your time budget, requirements and taste better.
 """
 
-# + [markdown]
+# %% [markdown]
 """
 ## **Exercise 02.4** Learning Curves
 
 Use the `results` object to plot the loss curves of your model! This is often a helpful visualisation to judge if longer training will make sense or to discover overfitting.
 """
 
-# + jupyter={"source_hidden": true}
+# %% jupyter={"source_hidden": true}
 # Solution 02.4
 f, ax = plt.subplots(1, 2, figsize=(10, 4), sharex=True)
 
@@ -436,6 +444,5 @@ ax[1].legend()
 
 f.suptitle("CNN Classification Learning Curves")
 f.savefig("mnist1d_classification_learning_curve.svg")
-# -
-
+# %%
 
